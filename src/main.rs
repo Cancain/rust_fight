3d6efd42bdba8fa382e8632a532ca0a391d8a7ba
Cancain <eriksson.tomas@gmail.com>
2019-07-07 15:31:09 +0200

    made a struct for characters

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,75 @@
 use std::io;
 
+struct Character {
+    name: String,
+    health: i32,
+    alive: bool,
+}
+
 fn main() {
     game();
 }
 
 fn game() {
     let mut game_on = true;
-    let mut character = String::from("");
+    let mut character = Character {
+        name: String::from(""),
+        health: 100,
+        alive: false,
+    };
+    let mut characters_created = 0;
 
     while game_on {
+        println!("{}", characters_created);
         println!("Welcome to rust_fight!");
         println!("Type [e]xit to quit");
 
         let mut command = String::from("");
 
-        if character.trim() == "" {
+        if character.alive == false {
             println!("You have no character, [c]reate to create a character");
         } else {
             println!("[f]ight to fight!");
         }
 
-        io::stdin().read_line(&mut command)
+        io::stdin()
+            .read_line(&mut command)
             .expect("Somthing went wrong");
-        
         println!("{}", command);
 
         if command.trim() == "e" || command.trim() == "exit" {
             game_on = false;
         } else if command.trim() == "c" || command.trim() == "create" {
-            character = create_character();
+            character = create_character(&mut characters_created);
         } else if command.trim() == "f" || command.trim() == "fight" {
             fight(&character);
         }
     }
 }
 
-fn create_character() -> String {
+fn create_character(characters_created: &mut i32) -> Character {
     println!("Please enter the name of your character");
 
     let mut name = String::from("");
-    io::stdin().read_line(&mut name)
+    io::stdin()
+        .read_line(&mut name)
         .expect("Something went wrong");
 
-    name
+    let character = Character {
+        name: name,
+        health: 100,
+        alive: true,
+    };
+
+    *characters_created += 1;
+
+    character
 }
 
-fn fight(player: &String) {
+fn fight(player: &Character) {
     let mut fight_on = true;
 
-    println!("Welcome to the areana {}!", player);
+    println!("Welcome to the areana {}!", player.name);
 
-    while fight_on {
-    }
+    while fight_on {}
 }
